e81e59d3935eeb5d2d5ef2318456933694791659
Sung2021 <90470359+Sung2021@users.noreply.github.com>
2021-09-17 13:38:58 -0400

    Create cite_seq_count.sh

---
+++ cite_seq_count.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+## for a single paired end (R1, R2) files
+path=~/Desktop//Sung_work/fastq/cell_hashing/19092-44
+CITE-seq-Count -R1 $path/19092FL-44-01-01-01_S64_L007_R1_001.fastq.gz -R2 $path/19092FL-44-01-01-01_S64_L007_R2_001.fastq.gz \
+-t ~/Desktop//Sung_work/fastq/cell_hashing/tag_list.csv \
+-cbf 1 -cbl 16 -umif 17 -umil 26 -cells 2000 \
+-o ~/Desktop//Sung_work/fastq/cell_hashing/output/
