4bf10938582fceb582790d31cddeae89651d02a2
Sung2021 <90470359+Sung2021@users.noreply.github.com>
2021-09-27 12:16:06 -0400

    Update cite_seq_count.sh

--- cite_seq_count.sh
+++ cite_seq_count.sh
@@ -2,6 +2,9 @@
 
 path=~/Desktop/Sung_work/fastq/cell_hashing/19092-44/01
 
+## all.R1.fastq, all.R2.fastq : the combinded 4 fastq files  
+## expected cell number : 10,000 
+
 CITE-seq-Count -R1 $path/all.R1.fastq.gz \
 -R2 $path/all.R2.fastq.gz \
 -t ~/Desktop/Sung_work/fastq/cell_hashing/tag_list.csv \
