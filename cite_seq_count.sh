0f44114f52a6f451be8b90edb8b00e5c60a66542
Sung2021 <90470359+Sung2021@users.noreply.github.com>
2021-10-15 16:04:37 -0400

    Update cite_seq_count.sh

--- cite_seq_count.sh
+++ cite_seq_count.sh
@@ -4,7 +4,7 @@ path=~/Desktop/Sung_work/fastq/cell_hashing/19092-44/01
 
 ## all.R1.fastq, all.R2.fastq : the combinded 4 fastq files  
 ## expected cell number : 10,000 
-## when checking fastq, tag sequence after 10 bp : --start trim 10 applied
+## when checking fastq, found that tag sequence starting after 10 bp : --start trim 10 applied
 
 CITE-seq-Count --start-trim 10 \
 -R1 $path/all.tag.R1.fastq.gz \
