4b8869a0dbed98f17435faf41b97a88bddccffcb
Sung2021 <90470359+Sung2021@users.noreply.github.com>
2022-04-07 10:19:25 -0400

    Update functions.r

--- R/analysis_format/functions.r
+++ R/analysis_format/functions.r
@@ -62,3 +62,28 @@ dm_pseudotime_gene <- function(gene, color='time'){
 dm_pseudotime_gene('Cxcr5', color='time')
 
 
+
+naive <- c('3','6')
+early_activated <- c('7','10','5','14')
+early_transitional <- c('8')
+later_transtional <- c('12','2','4')
+mature_Tfh <- c('0')
+non_Tfh_eff_memory_transitional <- c('13')
+non_Tfh_eff_memory_CD4 <- c('1')
+non_Tfh_eff_super_memory <- c('15')
+Tfh_mp2 <- c('16')
+Tfh_mp3 <- c('11')
+
+umap_groups <- function(groups){
+  meta <- obj.srt@meta.data
+  reduction <- obj.srt@reductions$umap@cell.embeddings %>% data.frame(check.names = F)
+  meta$select <- 'NA'
+  meta[meta$hhx_cluster %in% c(groups),]$select <- 'selected'
+  meta$select <- factor(meta$select)
+  p <- reduction %>% ggplot(aes(UMAP_1, UMAP_2, color=meta$select)) + 
+    geom_point(size=0.1) + theme_classic() +scale_color_manual(values = c('#EBEDEF',
+                                                                          '#EC7063'))
+  print(p)
+}
+
+umap_groups(mature_Tfh)
