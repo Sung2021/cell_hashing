e8475d14318c707c81e4bd660b4310d3d7838240
Sung2021 <90470359+Sung2021@users.noreply.github.com>
2022-04-15 09:37:00 -0400

    Create venn.r

---
+++ R/analysis_format/venn.r
@@ -0,0 +1,19 @@
+ggvenn::ggvenn(list(c11=c11,
+                    c16=c16,
+                    c9=c9))
+c11_over_c16 <- setdiff(c11,c16)
+all.markers %>% filter(gene %in% c11_over_c16) %>% write.csv('~/Desktop/c11_over-c16.csv')
+
+c16_over_c11 <- setdiff(c16,c11)
+
+all.markers %>% filter(gene %in% c16_over_c11) %>% write.csv('~/Desktop/c16_over-c11.csv')
+
+
+venn.output <- gplots::venn(list(c11=c11,
+                  c16=c16,
+                  c9=c9), intersections=TRUE)
+
+venn.output %>% str()
+venn.output <- venn.output %>% list()
+lengths(attributes(venn.output)$intersections)
+attributes(venn.output)$intersections$`c11:c9`
