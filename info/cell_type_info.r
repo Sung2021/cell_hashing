ec294ad3d052585bd1f6bde159cc0137e663d083
Sung2021 <90470359+Sung2021@users.noreply.github.com>
2022-06-03 11:22:08 -0400

    Create cell_type_info.r

---
+++ info/cell_type_info.r
@@ -0,0 +1,4 @@
+Tcmp <- c('Id3','Ccr7','Il7r','Lef1','Tcf7','Slamf6','Bcl2')
+Tfh <- c('Cxcr5','Bcl6','Ascl2','Pdcd1','Icos','Il21','Il4')
+Th1 <- c('Tbx21','Id2','Gzmb','Ifng','Ly6c2')
+Treg <- c('Foxp3','Il2ra','Il10','Cd81','Cd74','Klrg1')
