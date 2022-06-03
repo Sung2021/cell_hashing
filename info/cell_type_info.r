ed9ef8b99fd487b2f530d0cb5fe7cc4890df5bbb
Sung2021 <90470359+Sung2021@users.noreply.github.com>
2022-06-03 11:51:53 -0400

    Update cell_type_info.r

--- info/cell_type_info.r
+++ info/cell_type_info.r
@@ -2,3 +2,11 @@ Tcmp <- c('Id3','Ccr7','Il7r','Lef1','Tcf7','Slamf6','Bcl2')
 Tfh <- c('Cxcr5','Bcl6','Ascl2','Pdcd1','Icos','Il21','Il4')
 Th1 <- c('Tbx21','Id2','Gzmb','Ifng','Ly6c2')
 Treg <- c('Foxp3','Il2ra','Il10','Cd81','Cd74','Klrg1')
+
+Interleukins <- c("Il10","Il10ra", "Il10rb",
+                  "Il12a","Il12rb1" ,"Il12rb2",
+                 "Il2","Il2ra","Il2rb" , "Il2rg",
+                 "Il21" ,"Il21r",
+                 "Il4" , "Il4i1", "Il4ra",
+                 "Il6","Il6ra","Il6st" ,
+                 "Il7r")
