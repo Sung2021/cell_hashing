11b9f1dcdac5283dc870cad0b73c2abfeda18b5c
Sung2021 <90470359+Sung2021@users.noreply.github.com>
2022-06-16 15:03:04 -0400

    Update marker_gene_analysis.r

--- R/analysis_format/marker_gene_analysis.r
+++ R/analysis_format/marker_gene_analysis.r
@@ -68,3 +68,44 @@ genes <- c((markers %>% filter(avg_log2FC > 0) %>% top_n(20, avg_log2FC) %>% row
 DoHeatmap(obj.srt, features = genes, group.by = 'cell_type') + viridis::scale_fill_viridis() + 
   theme(axis.text.y = element_text(size = 7))
 
+
+
+## PPT figure update 22.06.16
+
+genes <- c('Il7r','Cxcr3','Ccr7','Sell','Tcf7','Gzma','Klrg1','Cx3cr1','Bhlhe40', 'Gzmb','Ccl5', 'Zeb2')
+FeaturePlot(obj.srt, features = genes, pt.size = 0.1, ncol =3 )
+FeaturePlot(obj.srt, features = 'pseudotime')
+
+tmp.genes <- read.csv('~/Desktop/tmp.csv', row.names = 1)
+blue <- tmp.genes$Tcm %>% as.vector()
+red <- tmp.genes$Tem %>% as.vector()
+
+markers <- read.csv('2022.Qiang.Tle3.scRNA/no.trimming.wt_only.1000.2.5.Tcm.Tem.mk.csv',
+                    row.names = 1)
+markers[1:5,]
+markers$color <- 'no significant'
+genes <- markers %>% filter(avg_log2FC > log2(1.5) & p_val_adj < 0.05) %>% rownames()
+markers[genes,]$color <- 'sig'
+genes <- markers %>% filter(avg_log2FC < -log2(1.5) & p_val_adj < 0.05) %>% rownames()
+markers[genes,]$color <- 'sig'
+markers[blue,]$color <- 'blue'
+markers[red,]$color <- 'red'
+markers %>% ggplot(aes(avg_log2FC, -log10(p_val_adj), color=color)) + 
+  geom_point(size=1, alpha=0.5) + geom_hline(yintercept = -log10(0.05), color='red') +
+  geom_vline(xintercept = c(-log2(1.5),log2(1.5)), color='red') +
+  xlim(c(-2,2)) +
+  theme_classic() + scale_color_manual(values = c('blue','grey','red','black'))
+
+markers$gene <- ''
+genes <- c(blue,red)
+markers[genes,]$gene <- genes
+markers %>% ggplot(aes(avg_log2FC, -log10(p_val_adj), label=gene)) + 
+  geom_point(size=1, alpha=0.1, aes(color=color)) + geom_hline(yintercept = -log10(0.05), color='red') +
+  theme_classic() + geom_text(vjust = 0.5, nudge_y = -0.5) + 
+  geom_vline(xintercept = c(-log2(1.5),log2(1.5)), color='red') + 
+  scale_color_manual(values = c('blue','grey','red','black')) +xlim(c(-2,2))
+
+genes <- c(tmp.genes[,1],tmp.genes[,2],tmp.genes[,3])
+DoHeatmap(obj.srt, features = genes, group.by = 'Identity') + 
+  theme(axis.text.y = element_text(size = 10)) 
+
