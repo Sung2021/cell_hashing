f057196e814211d4bf280fc969f1386bcf507b57
Sung2021 <90470359+Sung2021@users.noreply.github.com>
2022-09-06 14:19:12 -0400

    Update venn.r

--- R/analysis_format/venn.r
+++ R/analysis_format/venn.r
@@ -1,6 +1,16 @@
 ggvenn::ggvenn(list(c11=c11,
                     c16=c16,
                     c9=c9))
+Tcell.list = list(Tcm.enr=Tcm.enr,
+                  Tem.enr=Tem.enr,
+                  Tem.ko.up=Tem.ko.up,
+                  Tem.ko.dn=Tem.ko.dn,
+                  Tcm.ko.up=Tcm.ko.up,
+                  Tcm.ko.dn=Tcm.ko.dn)
+ggvenn::ggvenn(Tcell.list)
+
+
+
 c11_over_c16 <- setdiff(c11,c16)
 all.markers %>% filter(gene %in% c11_over_c16) %>% write.csv('~/Desktop/c11_over-c16.csv')
 
